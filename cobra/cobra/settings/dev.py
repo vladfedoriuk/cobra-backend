56719036f8d0244433b7f7ace46dc30dcb374b68
vladfedoriuk <vlad.fedoriuk2000@gmail.com>
2021-11-06 19:27:57 +0100

    settings adjustments

--- cobra/cobra/settings/dev.py
+++ cobra/cobra/settings/dev.py
@@ -1,1 +1,14 @@
+import os
+
 from .base import *
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB", "cobra"),
+        "USER": os.environ.get("POSTGRES_USER", "cobra"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "cobra"),
+        "HOST": os.environ.get("POSTGRES_HOST", "127.0.0.1"),
+        "PORT": os.environ.get("POSTGRES_PORT", "15432"),
+    }
+}
